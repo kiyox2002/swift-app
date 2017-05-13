195936a24f55f47344418301b144e61f52f43eac
Kiyoki Katou <kiyox20020924@gmail.com>
2017-05-13 13:38:44 +0900

    change 1 to 20

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Kiyoki Katou on 2017/05/13.
+//  Copyright © 2017 kiyox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        
+        if tapCount >= 20 {
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
